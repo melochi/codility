ad6ac9e7b561f6d513aac6375b269f50d2ea08f3
melochi <masha.khesin@gmail.com>
2016-05-20 14:36:37 -0400

    no right reversing

--- MaxSliceSwapRecursive.scala
+++ MaxSliceSwapRecursive.scala
@@ -5,7 +5,6 @@ object MaxSliceSwapRecursive {
 		//
 		var left  = leftSliceSum(A, 1, Array(0,0), 0, Array(0,0))
 		var right = leftSliceSum(A.reverse, 1, Array(0,0), 0, Array(0,0))
-		right = (right._1.reverse, right._2.reverse)
 
 		var maxSum = maxCombSum(left, right, 0, 0)
 
@@ -21,7 +20,7 @@ object MaxSliceSwapRecursive {
 	// element to the left of the slice:
 	// for each A(i) left slices are either empty or end at position A(i-1)
 	//
-    @tailrec def leftSliceSum(A: Array[Int], i: Int, lss: Array[Int], maxLeft: Int, comb: Array[Int]): (Array[Int], Array[Int]) = {
+	@tailrec def leftSliceSum(A: Array[Int], i: Int, lss: Array[Int], maxLeft: Int, comb: Array[Int]): (Array[Int], Array[Int]) = {
 		if( i <= A.length ) {
 			var a   = A(i-1)
 			var sum = math.max(lss(i) + a, 0)
@@ -39,8 +38,9 @@ object MaxSliceSwapRecursive {
 	// Here we assume that all argument arrays in both tuples are of the same length
 	// 
 	@tailrec def maxCombSum(left: (Array[Int], Array[Int]), right: (Array[Int], Array[Int]), i: Int, maxSum: Int): Int = {
-		if(i < left._1.length) {
-			var mx = math.max(maxSum, math.max(left._1(i)+right._2(i), left._2(i)+right._1(i)))
+		var N = left._1.length
+		if(i < N) {
+			var mx = math.max(maxSum, math.max(left._1(i)+right._2(N-1-i), left._2(i)+right._1(N-1-i)))
 			maxCombSum(left, right, i+1, mx)
 		} else {
 			maxSum
